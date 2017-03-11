6a2feffcd782270dcd11d9c38eef1e270402040f
harroyo0610 <humberto-_-lzc@hotmail.com>
2017-03-10 19:19:18 -0600

    examen3

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,8 +5,7 @@ class FlightsController < ApplicationController
 
   def search
     data_user = [params[:home][:from], params[:home][:to], params[:home][:passengers],  params[:home][:date]]
-
-    redirect_to controller: 'flights', action: 'index', data: data_user
+      redirect_to controller: 'flights', action: 'index', data: data_user
   end
 
   def index
@@ -14,7 +13,7 @@ class FlightsController < ApplicationController
     from = data_user[0]
     to = data_user[1]
     date = data_user[3]
-    passengers = data_user[2].to_i
+    @passengers = data_user[2].to_i
     @flights_avaiable = Flight.where(date: date, to: to, from: from)
   end
 
@@ -23,14 +22,55 @@ class FlightsController < ApplicationController
   end
 
   def new
-    
+    @passengers = []
+    @passengers << params[:passengers].to_i
+    flight = Flight.find(params[:id])
+    @passengers << flight
   end
 
   def create
-    
+    array_comprador = []
+    array_users = []
+    counter = 1
+    array_new = []
+    b = Booking.create(flight_id: params[:id].to_i, reservation_number: rand(0..10000), total: 0)
+    params.each do |key, value|
+      name = "bookin" + counter.to_s
+      email = "booking" + counter.to_s
+      if key == name
+         array_new << value
+      elsif key == email
+         array_new << value
+         array_users << array_new
+         array_new = []
+         counter += 1 
+      elsif key == "name_c"
+        array_comprador << value
+      elsif key == "email_c"
+        array_comprador << value
+        @user = User.create(name: array_comprador[0], email: array_comprador[1])
+        flight = Flight.find(params[:id]) 
+        UserFlight.create(user_id: @user.id, flight_id: flight.id) 
+        passengers = flight.passengers - params[:passengers].to_i
+        flight.update(passengers: passengers)
+        @booking = Booking.last
+        p "**"*50
+        p @user
+        user_booking = UserBooking.create(user_id: @user.id, booking_id: @booking.id)   
+        p "-" * 50      
+        p user_booking
+      end  
+    end
+    array_users.each do |user|
+      User.create(name: user[0], email: user[1])
+    end
+    total = array_users.count * Flight.find(params[:id]).cost
+    b.update(total: total)
+    redirect_to controller: 'flights', action: 'buy', data: @booking
   end
 
   def buy
-    
+     @booking = Booking.find(params[:data].to_i)
+     @flight = @booking.flight
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :flights, through: :user_flights
+  has_many :user_bookings
+  has_many :user_flights
   has_many :bookings, through: :user_bookings
+  validates :email, uniqueness: true
 end
